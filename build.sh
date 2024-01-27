b0e3f72f118e9e078ed44becc0a78d0b4651e405
nithin1217 <108689346+nithin1217@users.noreply.github.com>
2024-01-28 00:22:30 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build -t nithin2023/node:main .
+docker build -t nithin2023/node:dev .
 
-docker login -u nithin2023
+docker login -u="nithin2023" -p="Devops@123"
 
-docker push nithin2023/node:main
+docker push nithin2023/node:dev
