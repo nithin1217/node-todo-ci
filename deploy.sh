6d7cbde82116f06daea04b0dfb538dd57c086215
nithin1217 <108689346+nithin1217@users.noreply.github.com>
2024-01-28 02:12:10 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker pull nithin2023/node:dev
+docker pull nithin2023/node:one
 
-docker run -d --name node-todo-app-containerone -p 8000:8000 nithin2023/node:dev
+docker run -d --name node-todo-app-containerthree -p 8000:8000 nithin2023/node:one
